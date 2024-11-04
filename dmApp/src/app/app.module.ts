ec4cdfa0fd5ec84b1d5fefad1f5c785fd9244263
Sethuramalingam G <287952@ust.com>
2024-11-04 15:34:17 +0530

    Built Login Page

--- dmApp/src/app/app.module.ts
+++ dmApp/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SosComponent } from './sos/sos.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SosComponent
+    SosComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
