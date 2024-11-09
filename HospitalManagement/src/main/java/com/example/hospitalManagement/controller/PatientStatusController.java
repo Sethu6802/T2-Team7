a5b5955f5b95eb1b62b38d2b77a5cc7412055e8c
SANDRA JOHN <75668581+Sandra1709@users.noreply.github.com>
2024-11-09 12:57:48 +0530

    Updating Services-1

--- HospitalManagement/src/main/java/com/example/hospitalManagement/controller/PatientStatusController.java
+++ HospitalManagement/src/main/java/com/example/hospitalManagement/controller/PatientStatusController.java
@@ -3,7 +3,6 @@ package com.example.hospitalManagement.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +15,6 @@ import com.example.hospitalManagement.service.PatientStatusService;
 
 @RestController
 @RequestMapping("/patient-status")
-@CrossOrigin(origins = "http://localhost:8096")
 public class PatientStatusController {
 	
 	@Autowired
